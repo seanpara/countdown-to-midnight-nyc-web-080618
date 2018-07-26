e4aca771f4cc7936cdffd3ac13198b36a6eda5eb
Sean Logan Para <spara77@gmail.com>
2018-07-26 19:19:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,7 @@ def countdown_with_sleep
    while seconds_to_midnight > 0 
     puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1 
+     sleep(1)
   end 
   "HAPPY NEW YEAR!"
 end 
